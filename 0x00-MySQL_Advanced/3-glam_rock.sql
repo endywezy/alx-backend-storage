0ef1e7860fb4cb270571601689ed3dc68ec551b5
endywezy <endywezy@gmail.com>
2024-08-11 04:51:24 +0100

    chore: Update glam rock SQL query to use current year instead of hard-coded value

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,13 @@
--- Lists all bands with Glam rock as their main style, ranked by their longevity.
--- SELECT band_name, (IFNULL(split, YEAR(CURRENT_DATE())) - formed) AS lifespan
-SELECT band_name, (IFNULL(split, 2020) - formed) AS lifespan
-    FROM metal_bands
-    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
-    ORDER BY lifespan DESC;
+-- 3-glam_rock.sql
+-- This script lists all bands with Glam rock as their main style, ranked by their longevity.
+-- It uses the `formed` and `split` columns to compute the lifespan of each band.
+
+SELECT 
+    band_name, 
+    (IFNULL(split, 2022) - formed) AS lifespan
+FROM 
+    metal_bands
+WHERE 
+    FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+ORDER BY 
+    lifespan DESC;
