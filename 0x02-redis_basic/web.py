d045b1c5c370a464fe9ac6df0127e704b217c604
endywezy <endywezy@gmail.com>
2024-08-15 03:12:47 +0100

    refactor: Remove trailing whitespace in exercise.py and web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,24 +1,19 @@
 #!/usr/bin/env python3
-'''A module with tools for request caching and tracking.
-'''
+'''A module with tools for request caching and tracking.'''
+
 import redis
 import requests
 from functools import wraps
 from typing import Callable
 
-
+# Create a Redis connection instance
 redis_store = redis.Redis()
-'''The module-level Redis instance.
-'''
-
 
 def data_cacher(method: Callable) -> Callable:
-    '''Caches the output of fetched data.
-    '''
+    '''Caches the output of fetched data.'''
     @wraps(method)
-    def invoker(url) -> str:
-        '''The wrapper function for caching the output.
-        '''
+    def invoker(url: str) -> str:
+        '''The wrapper function for caching the output.'''
         redis_store.incr(f'count:{url}')
         result = redis_store.get(f'result:{url}')
         if result:
@@ -28,7 +23,6 @@ def data_cacher(method: Callable) -> Callable:
         redis_store.setex(f'result:{url}', 10, result)
         return result
     return invoker
-
 
 @data_cacher
 def get_page(url: str) -> str:
