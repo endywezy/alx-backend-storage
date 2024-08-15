a7e241d80ffab3f08fbe05cd665dc7b8aa9f700d
endywezy <endywezy@gmail.com>
2024-08-15 03:28:45 +0100

    refactor: Update track_get_page decorator to use Redis for caching

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,44 +1,36 @@
 #!/usr/bin/env python3
-'''A module with tools for request caching and tracking.'''
-
+"""
+Caching request module
+"""
 import redis
 import requests
 from functools import wraps
 from typing import Callable
 
-# Create a Redis connection instance
-redis_store = redis.Redis()
 
-def cache_page(method: Callable) -> Callable:
-    '''Decorator to cache the result of a URL request.'''
-    @wraps(method)
+def track_get_page(fn: Callable) -> Callable:
+    """ Decorator for get_page
+    """
+    @wraps(fn)
     def wrapper(url: str) -> str:
-        '''Wrapper function to manage caching and counting.'''
-        # Increment the count of the URL access
-        redis_store.incr(f'count:{url}')
-        
-        # Try to get the cached result
-        cached_result = redis_store.get(f'result:{url}')
-        if cached_result:
-            return cached_result.decode('utf-8')
-        
-        # If not cached, fetch the content from the URL
-        result = method(url)
-        
-        # Cache the result with an expiration time of 10 seconds
-        redis_store.setex(f'result:{url}', 10, result)
-        
-        return result
+        """ Wrapper that:
+            - check whether a url's data is cached
+            - tracks how many times get_page is called
+        """
+        client = redis.Redis()
+        client.incr(f'count:{url}')
+        cached_page = client.get(f'{url}')
+        if cached_page:
+            return cached_page.decode('utf-8')
+        response = fn(url)
+        client.set(f'{url}', response, 10)
+        return response
     return wrapper
 
-@cache_page
+
+@track_get_page
 def get_page(url: str) -> str:
-    '''Fetches the HTML content of a URL and caches it.'''
+    """ Makes a http request to a given endpoint
+    """
     response = requests.get(url)
     return response.text
-
-if __name__ == "__main__":
-    # Test with a slow URL to check caching
-    test_url = "http://slowwly.robertomurray.co.uk/delay/5000/url/http://www.google.com"
-    print(get_page(test_url))
-    print(get_page(test_url))  # Should return the cached result
